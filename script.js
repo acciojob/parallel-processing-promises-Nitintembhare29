59a1bd531ab8cf04884b3354215f7e9b07692335
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-05-01 15:02:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImages(){
+	const promises = images.map(url =>{
+		const img = new Image();
+		img.onload = () => resolve(img);
+		img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);		
+	})
+}
+
+Promise.all(promises)
+.then(images =>{
+	const outputDiv = document.getElementById("output");
+	images.forEach(image =>{
+		outputDiv.appendChild(image);
+	})
+})
+.catch(error =>{
+	console.log(error);
+})
+document.getElementById("download-images-button").addEventListener("click", downloadImages);
